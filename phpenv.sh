7a3c833299fb6625aa224a3a28411c50760315df
yagas <yagas@users.noreply.github.com>
2019-08-12 10:35:09 +0800

    Update phpenv.sh

--- phpenv.sh
+++ phpenv.sh
@@ -34,8 +34,7 @@ function checkrpm()
 	echo -e $message
 }
 
-# check directory exist.
-# if not. than create.
+# 检测目录是否存在
 if [ ! -d $unpack ]; then
 	mkdir $unzipdir
 fi
@@ -55,23 +54,23 @@ checkrpm gd-devel
 checkrpm pcre2-devel
 checkrpm freetype-devel
 
-# install rpm package.
-if [ "$packages" != "" ]; then
+# 安装文件包
+if [ ! -z $packages ]; then
 	yum -y install $packages
 fi
 
-# check libmcrypt.so.
+# 判断libmcrypt是否安装，没安装则自动下载安装包
 libmcrypt=$(whereis libmcrypt.so | grep libmcrypt.so | wc -l)
-if [ "$libmcrypt" -neq "1" ]; then
+if [ $libmcrypt -neq 1 ]; then
 	if [ ! -f $software/libmcrypt-2.5.8.tar.gz ]; then
 		echo -e "\n\ndownload libmcrypt package ...\n"
 		curl -o $software/libmcrypt-2.5.8.tar.gz -L https://sourceforge.net/projects/mcrypt/files/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz/download
 	fi
 fi
 
-# check php-5.6.40
+# 判断php-5.6.40是否安装，没安装则自动下载安装包
 php=$(whereis php | grep php | wc -l)
-if [ "$php" -neq "1" ]; then
+if [ $libmcrypt -ne 1 ]; then
 	if [ ! -f $software/php-5.6.40.tar.gz ]; then
 		echo -e "\n\ndownload php-5.6.40 package ...\n"
 		curl -o $software/php-5.6.40.tar.gz -L https://www.php.net/distributions/php-5.6.40.tar.gz
@@ -80,7 +79,7 @@ fi
 
 
 
-if [ $libmcrypt -neq 1 ]; then
+if [ $libmcrypt -ne 1 ]; then
 	tar xzf $software/libmcrypt-2.5.8.tar.gz -C $unzipdir
 	cd $unzipdir/libmcrypt-2.5.8
 	./configure
@@ -88,10 +87,12 @@ if [ $libmcrypt -neq 1 ]; then
 	sudo make install
 fi
 
-if [ $php -neq 1 ]; then
+if [ $php -ne 1 ]; then
 	tar xzf $software/php-5.6.40.tar.gz -C $unzipdir
 	cd $unzipdir/php-5.6.40
 	./configure --enable-fpm --with-freetype-dir --with-jpeg-dir --with-png-dir --with-zlib-dir --with-libxml-dir --enable-bcmath --enable-shmop --enable-sysvsem --enable-inline-optimization --with-curl --enable-mbregex --enable-mbstring --with-mhash --with-gd --enable-gd-native-ttf --enable-pcntl --enable-zip --with-bz2 --without-iconv --with-gettext --with-pear --enable-calendar --with-pdo-mysql --enable-opcache --with-openssl --with-mcrypt --prefix=/usr/local/php56
 	make -j4
 	sudo make install
 fi
+
+echo "\nDone.\n"
