4752658fc986f88d6d78b3d329e97c69cddcbff6
yagas <yagas@users.noreply.github.com>
2019-08-12 10:14:03 +0800

    upgrade, check and install php

--- phpenv.sh
+++ phpenv.sh
@@ -14,6 +14,8 @@ echo "copyright (c) 2019 Manyan Network Technology Co. Ltd."
 echo -e "\n\n"
 
 packages=""
+unzipdir='./unpack'
+software='./software'
 
 function checkrpm()
 {
@@ -32,11 +34,15 @@ function checkrpm()
 	echo -e $message
 }
 
-# 安装文件包
-for pkg in $packages
-do
-	yum -y install $pkg
-done
+# check directory exist.
+# if not. than create.
+if [ ! -d $unpack ]; then
+	mkdir $unzipdir
+fi
+
+if [ ! -d $software ]; then
+	mkdir $software
+fi
 
 checkrpm gcc
 checkrpm autoconf
@@ -49,23 +55,43 @@ checkrpm gd-devel
 checkrpm pcre2-devel
 checkrpm freetype-devel
 
-echo $packages
-
-# 检测目录是否存在
-if [ ! -d ./software ]; then
-	mkdir ./software
+# install rpm package.
+if [ $packages != '' ]; then
+	yum -y install $packages
 fi
 
-# 下载文件包
-if [ ! -f ./software/libmcrypt-2.5.8.tar.gz ]; then
-	echo -e "\n\ndownload libmcrypt package ...\n"
-	curl -o ./software/libmcrypt-2.5.8.tar.gz -L https://sourceforge.net/projects/mcrypt/files/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz/download
+# check libmcrypt.so.
+libmcrypt=$(whereis libmcrypt.so | grep libmcrypt.so | wc -l)
+if [ $libmcrypt -neq 1 ]; then
+	if [ ! -f $software/libmcrypt-2.5.8.tar.gz ]; then
+		echo -e "\n\ndownload libmcrypt package ...\n"
+		curl -o $software/libmcrypt-2.5.8.tar.gz -L https://sourceforge.net/projects/mcrypt/files/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz/download
+	fi
 fi
 
-unzipdir='unpack'
-mkdir $unzipdir
-tar xzf ./software/libmcrypt-2.5.8.tar.gz -C $unzipdir
-cd $unzipdir/libmcrypt-2.5.8
-./configure
-make -j4
-sudo make install
+# check php-5.6.40
+php=$(whereis php | grep php | wc -l)
+if [ $libmcrypt -neq 1 ]; then
+	if [ ! -f $software/php-5.6.40.tar.gz ]; then
+		echo -e "\n\ndownload php-5.6.40 package ...\n"
+		curl -o $software/php-5.6.40.tar.gz -L https://www.php.net/distributions/php-5.6.40.tar.gz
+	fi
+fi
+
+
+
+if [ $libmcrypt -neq 1 ]; then
+	tar xzf $software/libmcrypt-2.5.8.tar.gz -C $unzipdir
+	cd $unzipdir/libmcrypt-2.5.8
+	./configure
+	make -j4
+	sudo make install
+fi
+
+if [ $php -neq 1 ]; then
+	tar xzf $software/php-5.6.40.tar.gz -C $unzipdir
+	cd $unzipdir/php-5.6.40
+	./configure --enable-fpm --with-freetype-dir --with-jpeg-dir --with-png-dir --with-zlib-dir --with-libxml-dir --enable-bcmath --enable-shmop --enable-sysvsem --enable-inline-optimization --with-curl --enable-mbregex --enable-mbstring --with-mhash --with-gd --enable-gd-native-ttf --enable-pcntl --enable-zip --with-bz2 --without-iconv --with-gettext --with-pear --enable-calendar --with-pdo-mysql --enable-opcache --with-openssl --with-mcrypt --prefix=/usr/local/php56
+	make -j4
+	sudo make install
+fi
