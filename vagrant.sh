43c6c23a9fe44333c89a8b02793e602af40ca3e0
yagas <yagas@163.com>
2019-08-15 19:47:03 +0800

    auto install for yum

---
+++ vagrant.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+yum -y install yum-utils yum-config-manager
+rpm -Uvh https://mirrors.tuna.tsinghua.edu.cn/remi/enterprise/remi-release-7.rpm
+
+cat >> /etc/yum.repos.d/nginx.repo <<EOF
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+
+[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key
+EOF
+
+yum clean all && yum makecache
