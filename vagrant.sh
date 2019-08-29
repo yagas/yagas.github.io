e87cc7247b7367faa5c878b08248af7ee43d12e7
yagas <yagas@163.com>
2019-08-29 09:17:32 +0800

    fixed: Do not translate slashes

--- vagrant.sh
+++ vagrant.sh
@@ -4,7 +4,7 @@ yum -y install yum-utils yum-config-manager
 rpm -vih http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
 rpm -Uvh https://mirrors.tuna.tsinghua.edu.cn/remi/enterprise/remi-release-7.rpm
 
-cat >> /etc/yum.repos.d/nginx.repo <<EOF
+cat >> /etc/yum.repos.d/nginx.repo <<\EOF
 [nginx-stable]
 name=nginx stable repo
 baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
@@ -20,7 +20,6 @@ enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key
 EOF
 
-sed -i 's/\/\/\//\//' /etc/yum.repos.d/nginx.repo
 yum clean all && yum makecache
 yum-config-manager --enable nginx-stable
 yum-config-manager --enable remi-php56
