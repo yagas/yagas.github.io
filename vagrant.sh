71b83a93c4acff646eb06329330273ac93d54299
yagas <yagas@163.com>
2019-09-03 12:01:09 +0800

    fixed: repo

--- vagrant.sh
+++ vagrant.sh
@@ -24,5 +24,5 @@ EOF
 yum clean all && yum makecache
 yum-config-manager --enable nginx-stable
 yum-config-manager --enable remi-php56
-yum --enablerepo=remi56 install php php-fpm php-mongodb php-gd php-redis
+yum --enablerepo=remi-php56 install php php-fpm php-mongodb php-gd php-redis
 yum install nginx
