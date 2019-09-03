f487e6358440cbcaeabf1b698a83d62396230aa6
yagas <yagas@163.com>
2019-09-03 14:57:53 +0800

    update curl

--- vagrant.sh
+++ vagrant.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+yum update curl
 yum -y install yum-utils yum-config-manager
 rpm --import https://rpms.remirepo.net/RPM-GPG-KEY-remi
 rpm -vih http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
